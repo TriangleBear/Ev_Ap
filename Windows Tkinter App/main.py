f5406aeadf890f8835abdfd02123389603fc0716
TriangleBear <kakanini31@gmail.com>
2025-01-26 04:48:01 +0800

    Update main.py

--- Windows Tkinter App/main.py
+++ Windows Tkinter App/main.py
@@ -1,8 +1,6 @@
 from rfid_app import MyApp
 import customtkinter as CTk
 from CTkMessagebox import CTkMessagebox
-import os
-import sys
 
 class RFIDApp(MyApp):
     def __init__(self):
@@ -12,14 +10,5 @@ class RFIDApp(MyApp):
         super().run()
 
 if __name__ == "__main__":
-    # Get the path to the icon file
-    if hasattr(sys, '_MEIPASS'):
-        icon_path = os.path.join(sys._MEIPASS, 'icon64.ico')
-    else:
-        icon_path = 'D:/Programming/AHO/RFID App/ORG-RFID-EVENTS/icon64.ico'
-    
-    root = CTk.CTk()
-    root.iconbitmap(icon_path)
-    
     app = RFIDApp()
     app.run()
